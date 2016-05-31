0f880b528c4ac54879480021931955d73f625f43
Baoqiang Lao <lbq@shao.ac.cn>
2016-05-31 10:38:59 +0800

    build mwapipeline physical graph and deploy it to a compute node

---
+++ build_mwapipeline_graph.py
@@ -0,0 +1,76 @@
+#
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+#    created by Baoqiang Lao
+#    SHAO
+#    27.05.2016
+"""
+Build the physical graph
+"""
+
+from build_mwapipeline_graph_common import AbstractBuildGraph
+import json
+import optparse
+import sys
+
+parser =optparse.OptionParser()
+parser.add_option("-o","--obsnum",action="store",dest="obsnum",help="The obsnum is the observation ID",default="1123785280")
+parser.add_option("-p","--proj",action="store",dest="proj",help="The proj is the project ID",default="G0008")
+parser.add_option("-m","--model",action="store",dest="model",help="The model is the calibration model",default="3c444")
+parser.add_option("-t","--tgtobsnum",action="store",dest="tgtobsnum",help="The tgtobsnum is the target observation ID",default="1123785280")
+parser.add_option("-n","--node-id",action="store",dest="node_id",help="The node where the graph will be deployed",default="localhost")
+
+(opts,args)=parser.parse_args(sys.argv)
+
+obsnum=opts.obsnum
+proj=opts.proj
+bashdir='MWABASECODEdir/bin'
+model=opts.model
+tgt_obsnum=opts.tgtobsnum
+node_id=opts.node_id
+
+absbg=AbstractBuildGraph('bucket_name','shutdown',{'0':"0.0.0.0"},'','session_id')
+#app drop
+drop_bash_obsdownloads=absbg.create_bash_shell_app(node_id,'obsdownloads','bash {0}/grab_cot.sh {1} {2}'.format(bashdir,obsnum,proj))
+drop_bash_calibrate=absbg.create_bash_shell_app(node_id,'calibrate','bash {0}/calibrate.sh {1} {2} {3}'.format(bashdir,obsnum,proj,model))
+drop_bash_imaging=absbg.create_bash_shell_app(node_id,'imaging','bash {0}/imaging.sh {1} {2} {3} {4}'.format(bashdir,obsnum,proj,model,tgt_obsnum))
+drop_bash_self_calibration=absbg.create_bash_shell_app(node_id,'self_calibration','bash {0}/selfcal.sh {1} {2}'.format(bashdir,obsnum,proj))
+#memory drop
+drop_memory_ms=absbg.create_memory_drop(node_id,'MS file')
+drop_memory_bin=absbg.create_memory_drop(node_id,'solutions bin file')
+drop_memory_imgfits=absbg.create_memory_drop(node_id,'imaging fits file')
+drop_memory_self_calibration_fits=absbg.create_memory_drop(node_id,'self calibration fits file')
+#link drop
+drop_bash_obsdownloads.addOutput(drop_memory_ms)
+drop_bash_calibrate.addInput(drop_memory_ms)
+drop_bash_calibrate.addOutput(drop_memory_bin)
+drop_bash_imaging.addInput(drop_memory_bin)
+drop_bash_imaging.addOutput(drop_memory_imgfits)
+drop_bash_self_calibration.addInput(drop_memory_ms)
+drop_bash_self_calibration.addOutput(drop_memory_self_calibration_fits)
+
+absbg.start_oids.append(drop_bash_obsdownloads['uid'])
+drop_list=absbg.drop_list
+
+with open("{0}.json".format(obsnum), 'w') as f:
+    json.dump(drop_list, f,indent=2)
+
+
---
+++ build_mwapipeline_graph_common.py
@@ -0,0 +1,254 @@
+#
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+#    modify by Baoqiang Lao
+#    SHAO
+#    26.5.2016
+
+"""
+The abstract graph builder for MWA Pipeline
+"""
+import os
+import uuid
+from abc import ABCMeta, abstractmethod
+
+#from aws_chiles02.apps_general import CopyLogFilesApp
+#from aws_chiles02.common import get_module_name
+from dfms.apps.bash_shell_app import BashShellApp
+from dfms.drop import dropdict, DirectoryContainer, BarrierAppDROP
+
+
+class AbstractBuildGraph:
+    # This ensures that:
+    #  - This class cannot be instantiated
+    #  - Subclasses implement methods decorated with @abstractmethod
+    __metaclass__ = ABCMeta
+
+    def __init__(self, bucket_name, shutdown, node_details, volume, session_id):
+        self._drop_list = []
+        self._start_oids = []
+        self._map_carry_over_data = {}
+        self._bucket_name = bucket_name
+        self._shutdown = shutdown
+        self._bucket = None
+        self._node_details = node_details
+        self._volume = volume
+        self._session_id = session_id
+        self._counters = {}
+
+        #for key, list_ips in self._node_details.iteritems():
+        #    for instance_details in list_ips:
+        #        self._map_carry_over_data[instance_details['ip_address']] = self.new_carry_over_data()
+
+    @property
+    def drop_list(self):
+        return self._drop_list
+
+    @property
+    def start_oids(self):
+        return self._start_oids
+
+    def add_drop(self, drop):
+        self._drop_list.append(drop)
+
+    def get_oid(self, count_type):
+        count = self._counters.get(count_type)
+        if count is None:
+            count = 1
+        else:
+            count += 1
+        self._counters[count_type] = count
+
+        return '{0}__{1:06d}'.format(count_type, count)
+
+    @staticmethod
+    def get_uuid():
+        return str(uuid.uuid4())
+
+    #def copy_logfiles_and_shutdown(self):
+    #    """
+    #    Copy the logfile to S3 and shutdown
+    #    """
+    #    for list_ips in self._node_details.values():
+    #        for instance_details in list_ips:
+    #            node_id = instance_details['ip_address']
+
+    #            copy_log_drop = self.create_app(node_id, get_module_name(CopyLogFilesApp), 'copy_log_files_app')
+
+    #            # After everything is complete
+    #            for drop in self._drop_list:
+    #                if drop['type'] in ['plain', 'container'] and drop['node'] == node_id:
+    #                    copy_log_drop.addInput(drop)
+
+    #            s3_drop_out = self.create_s3_drop(
+    #                node_id,
+    #                self._bucket_name,
+    #                '{0}/{1}.tar'.format(
+    #                    self._session_id,
+    #                    node_id,
+    #                ),
+    #                'aws-chiles02',
+    #                oid='s3_out'
+    #            )
+    #            copy_log_drop.addOutput(s3_drop_out)
+
+    #            if self._shutdown:
+    #                shutdown_drop = self.create_bash_shell_app(node_id, 'sudo shutdown -h +5 "DFMS node shutting down" &')
+    #                shutdown_drop.addInput(s3_drop_out)
+
+    def tag_all_app_drops(self, tags):
+        for drop in self._drop_list:
+            if drop['type'] == 'app':
+                drop.update(tags)
+
+    #@abstractmethod
+    #def build_graph(self):
+    #    """
+    #    Build the graph
+    #    """
+
+    #@abstractmethod
+    #def new_carry_over_data(self):
+    #    """"
+    #    Get the carry over data structure
+    #    """
+
+    def create_memory_drop(self, node_id, name, oid='memory_drop'):
+        drop = dropdict({
+            "type": 'plain',
+            "storage": 'memory',
+            "oid": self.get_oid(oid),
+            "uid": self.get_uuid(),
+            "nm": name,
+            "node": node_id,
+        })
+        self.add_drop(drop)
+        return drop
+
+    def get_module_name(self,item):
+        return item.__module__ + '.' + item.__name__
+
+    def create_bash_shell_app(self, node_id, name, command, oid='bash_shell_app', input_error_threshold=100):
+        drop = dropdict({
+            "type": 'app',
+            "app": self.get_module_name(BashShellApp),
+            "oid": self.get_oid(oid),
+            "uid": self.get_uuid(),
+            "command": command,
+            "input_error_threshold": input_error_threshold,
+            "nm": name,
+            "node": node_id,
+        })
+        self.add_drop(drop)
+        return drop
+
+    def create_barrier_app(self, node_id, oid='barrier_app', input_error_threshold=100):
+        drop = dropdict({
+            "type": 'app',
+            "app": self.get_module_name(BarrierAppDROP),
+            "oid": self.get_oid(oid),
+            "uid": self.get_uuid(),
+            "input_error_threshold": input_error_threshold,
+            "node": node_id,
+        })
+        self.add_drop(drop)
+        return drop
+
+    def create_app(self, node_id, app, oid, input_error_threshold=100, **key_word_arguments):
+        drop = dropdict({
+            "type": 'app',
+            "app": app,
+            "oid": self.get_oid(oid),
+            "uid": self.get_uuid(),
+            "input_error_threshold": input_error_threshold,
+            "node": node_id,
+        })
+        drop.update(key_word_arguments)
+        self.add_drop(drop)
+        return drop
+
+    def create_docker_app(self, node_id, app, oid, image, command, user='ec2-user', input_error_threshold=100, **key_word_arguments):
+        drop = dropdict({
+            "type": 'app',
+            "app": app,
+            "oid": self.get_oid(oid),
+            "uid": self.get_uuid(),
+            "image": image,
+            "command": command,
+            "user": user,
+            "input_error_threshold": input_error_threshold,
+            "node": node_id,
+        })
+        drop.update(key_word_arguments)
+        self.add_drop(drop)
+        return drop
+
+    def create_directory_container(self, node_id, oid='directory_container', expire_after_use=True):
+        oid_text = self.get_oid(oid)
+        drop = dropdict({
+            "type": 'container',
+            "container": self.get_module_name(DirectoryContainer),
+            "oid": oid_text,
+            "uid": self.get_uuid(),
+            "precious": False,
+            "dirname": os.path.join(self._volume, oid_text),
+            "check_exists": False,
+            "expireAfterUse": expire_after_use,
+            "node": node_id,
+        })
+        self.add_drop(drop)
+        return drop
+
+    def create_s3_drop(self, node_id, bucket_name, key, profile_name, oid='s3'):
+        drop = dropdict({
+            "type": 'plain',
+            "storage": 's3',
+            "oid": self.get_oid(oid),
+            "uid": self.get_uuid(),
+            "expireAfterUse": True,
+            "precious": False,
+            "bucket": bucket_name,
+            "key": key,
+            "profile_name": profile_name,
+            "node": node_id,
+        })
+        self.add_drop(drop)
+        return drop
+
+    def create_json_drop(self, node_id, oid='json'):
+        oid_text = self.get_oid(oid)
+        drop = dropdict({
+            "type": 'plain',
+            "storage": 'json',
+            "oid": oid_text,
+            "uid": self.get_uuid(),
+            "precious": False,
+            "dirname": os.path.join(self._volume, oid_text),
+            "check_exists": False,
+            "node": node_id,
+        })
+        self.add_drop(drop)
+        return drop
+
+
+
+
+
